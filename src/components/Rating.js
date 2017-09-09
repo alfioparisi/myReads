75dbd5b313379a0443bc6e059474430bbbfe2320
Alfio Parisi <alfioparisi.93@gmail.com>
2017-09-09 16:13:54 +0100

    Fix prop types.

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -14,7 +14,10 @@ import PropTypes from 'prop-types';
 class Rating extends Component {
   static propTypes = {
     onClick: PropTypes.func.isRequired,
-    rating: PropTypes.string.isRequired
+    rating: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.number
+    ])
   }
 
   /**
